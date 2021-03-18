2f6a0617714b6440abd70454b515837f3ad731e3
Miłosz Lenczewski <milosz.lenczewski@landingi.com>
2021-03-18 11:18:09 +0100

    [PHP-79]Use symfony extension convention to autoload services (#3)

--- src/DependencyInjection/LandingiModularMonolithExtension.php
+++ src/DependencyInjection/LandingiModularMonolithExtension.php
@@ -8,11 +8,11 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class ModularMonolithExtension extends Extension
+final class LandingiModularMonolithExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.xml');
     }
 }
